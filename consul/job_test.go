afb005fe5388f340ebd992c4a903eaf2a1a31efe
George Dean <gdean@pivotal.io>
2016-03-30 10:43:18 -0700

    Moved SetJobInstanceCount from consul-release to destiny/consul
    [#101812768]
    
    Signed-off-by: Christian Ang <cang@pivotal.io>

---
+++ consul/job_test.go
@@ -0,0 +1,34 @@
+package consul_test
+
+import (
+	"github.com/pivotal-cf-experimental/destiny/consul"
+	"github.com/pivotal-cf-experimental/destiny/iaas"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Job", func() {
+	Describe("SetJobInstanceCount", func() {
+		It("sets the correct values for instances and static_ips given a count", func() {
+			manifest := consul.NewManifest(consul.Config{
+				IPRange: "10.244.4.0/24",
+			}, iaas.NewWardenConfig())
+			job := manifest.Jobs[0]
+			network := manifest.Networks[0]
+			properties := manifest.Properties
+
+			Expect(job.Instances).To(Equal(1))
+			Expect(job.Networks[0].StaticIPs).To(HaveLen(1))
+			Expect(job.Networks[0].Name).To(Equal(network.Name))
+			Expect(job.Networks[0].StaticIPs).To(Equal([]string{"10.244.4.4"}))
+			Expect(properties.Consul.Agent.Servers.Lan).To(Equal([]string{"10.244.4.4"}))
+
+			job, properties = consul.SetJobInstanceCount(job, network, properties, 3)
+			Expect(job.Instances).To(Equal(3))
+			Expect(job.Networks[0].StaticIPs).To(HaveLen(3))
+			Expect(job.Networks[0].StaticIPs).To(Equal([]string{"10.244.4.4", "10.244.4.5", "10.244.4.6"}))
+			Expect(properties.Consul.Agent.Servers.Lan).To(Equal([]string{"10.244.4.4", "10.244.4.5", "10.244.4.6"}))
+		})
+	})
+})
